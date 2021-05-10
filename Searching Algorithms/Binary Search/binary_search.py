b89c7bb16109181b82266f79635b737cd571665c
rahilsayed <83951702+rahilsayed@users.noreply.github.com>
2021-05-10 17:33:37 +0530

    Update binary_search.py

---
+++ Searching Algorithms/Binary Search/binary_search.py
@@ -0,0 +1,24 @@
+#Author: RahIL Sayed
+#Date: 05/10/2021
+
+def binary_search(arr,key):
+    start = 0
+    end = len(arr) - 1
+    mid = (start + end) // 2
+    if arr[mid] == key:
+        return mid
+    while end >= start:
+        mid = (start + end) // 2
+        if arr[mid] == key:
+            return mid
+        elif arr[mid] < key:
+            start = mid + 1
+        elif arr[mid] > key:
+            end = mid - 1
+
+#sample test case        
+arr= [10,12,15,19,25,30]
+print(binary_search(arr,11))
+
+#Time Complexity: O(log(n))
+#Space Complexity: O(1)
