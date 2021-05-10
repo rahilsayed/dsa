5894f9dc2641f349d403404afbe9b8ece1a30da0
rahilsayed <83951702+rahilsayed@users.noreply.github.com>
2021-05-10 15:58:13 +0530

    Create Searching Algorithms
    Created Searching Algorithms and updated folder and codes.

---
+++ Searching Algorithms/Linear Search/linear_search.py
@@ -0,0 +1,16 @@
+#Author: Rahil Sayed
+#Date: 05/10/2021
+
+def linear_search(arr,key):
+    for i in range(len(arr)):
+        if arr[i] == key:
+            position = i 
+            return position
+
+#sample test case
+arr = [1,0,2,9,3,84,7,5,6]
+key = 3
+print(linear_search(arr,key)) 
+
+#Time Complexity: O(n)
+#Space Complexity: O(1) 
